477bf5eae930ab4432c6c70f2431352611ef8601
Hansol YOO <78459995+yhansol145@users.noreply.github.com>
2023-01-03 23:50:08 +0900

    - 회원서비스 테스트 코드 추가

--- src/main/java/hello/hellospring/service/MemberService.java
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -11,7 +11,11 @@ import java.util.Optional;
 @Service
 public class MemberService {
 
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    private final MemberRepository memberRepository;
+
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
 
     /**
      * 회원 가입
